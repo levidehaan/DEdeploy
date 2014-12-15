d4f9b3ca4ebf286e10cec4d61ff8ad402ae20537
Levi DeHaan <levi@levidehaan.com>
2014-12-15 04:17:43 -0700

    Updates to code and readme

--- readme.md
+++ readme.md
@@ -5,3 +5,29 @@ Tools for Docker in the Enterprise
 
 
 This is a first commit of some tools used to build large data center installs of docker and tools like mesos/marathon
+
+
+What this project is going to cover
+------------------------------------
+
+
+Docker Enterprise tools are a companion set of tools for deploying Enterprise clusters/projects using a tool that simplifies deployment.
+This project is meant to cover deployment from bare metal up.
+
+
+
+What is left to do:
+-------------------
+
+Immediate:
+* Make networking config easier for switching between testing (virtualbox) && prod
+* setup littlechef book for base docker
+* setup littlechef book for base cluster setup (mesos/marathon/consul/registrator||mesos/marathon/kubernetes/cadvisor||etc..)
+* add to littlechef + fabfile to read from dhcp client list from dnsmasq to populate ip list for servers to run cookbooks on.
+
+
+Coming:
+* Base monitoring install (deployed with docker, as plugin, i.e. can be added later)
+* Add Chef more chef scripts
+* Add customized dockerfiles for mesos/marathon, kafka, hadoop, sparksql, etc..
+* Add cluster deploy scripts to fabric file, using fabrics parallel functionality
--- runme.sh
+++ runme.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
 
 INPUT=/tmp/menu.sh.$$
-
+>$INPUT
 OUTPUT=output.log
-touch $OUTPUT
+>$OUTPUT
+PXECID=/tmp/pxecontainer.id
+>$PXECID
 
 vi_editor=${EDITOR-vi}
 
 trap "rm $OUTPUT; rm $INPUT; exit" SIGHUP SIGINT SIGTERM
 
-function createPXEbooter(){
+function baseTools(){
+	sudo apt-get install --force-yes --yes bridge-utils
+}
 
+function createPXEbooter(){
     dialog --backtitle "Starting docker build!"\
      --title "Installing......" --clear\
       --infobox "Setting up your pxe installer :)" 40 90
 
-    sudo docker build -t pxeinstaller dockerfiles/pxebooter 2>&1 | tee -a output.log
+    sudo docker build -t pxeinstaller dockerfiles/pxebooter 2>&1 | tee -a $OUTPUT
 
-    sudo umount dockerfiles/pxebooter/isomount 2>&1 | tee -a output.log
-
-	dialog --backtitle "PXE Booter Install COMPLETED!!"\
-	 --title "Finished Install!" --clear\
+    dialog --backtitle "PXE Booter Install COMPLETED!!"\
+	 --title "Finished Install! Run PXE Booter Next" --clear\
 	 --msgbox "$(cat output.log)" 40 90
-
 }
 
 function downloadUbuntuServer(){
-
     dialog --backtitle "downloading ubuntu 14.04.1 server image"\
      --title "Downloading......" --clear\
       --infobox "Downloading Ubuntu Server, this could take a long time... :)" 40 90
 
-    wget http://mirror.pnl.gov/releases/14.04.1/ubuntu-14.04.1-server-amd64.iso 2>&1 | tee -a output.log
+    wget http://mirror.pnl.gov/releases/14.04.1/ubuntu-14.04.1-server-amd64.iso 2>&1 | tee -a $OUTPUT
 
     dialog --backtitle "image download COMPLETED!!"\
 	 --title "Please Run Mount Local Files next!" --clear\
 	 --msgbox "$(cat output.log)" 40 90
-
 }
+
 function MountLocalFiles() {
     dialog --backtitle "Unpacking ubuntu 14.04.1 server image"\
      --title "Unpacking......" --clear\
       --infobox "Unpacking Ubuntu Server, this could take a long time... :)" 40 90
 
-	mkdir dockerfiles/pxebooter/isomount; mkdir dockerfiles/pxebooter/installfiles 2>&1 | tee -a output.log
+	mkdir dockerfiles/pxebooter/isomount; mkdir dockerfiles/pxebooter/installfiles 2>&1 | tee -a $OUTPUT
 
-	sudo mount -o loop `pwd`/ubuntu-14.04.1-server-amd64.iso dockerfiles/pxebooter/isomount 2>&1 | tee -a output.log
+	sudo mount -o loop `pwd`/ubuntu-14.04.1-server-amd64.iso dockerfiles/pxebooter/isomount 2>&1 | tee -a $OUTPUT
 
     sudo cp -fr dockerfiles/pxebooter/isomount/install/netboot/* dockerfiles/pxebooter/installfiles/
 
@@ -54,10 +55,75 @@ function MountLocalFiles() {
 	 --msgbox "$(cat output.log)" 40 90
 }
 
+function unMountLocalFiles() {
+    dialog --backtitle "removing mounts"\
+     --title "unmounting......" --clear\
+      --infobox "Unmounting filesystems" 40 90
+
+	sudo umount dockerfiles/pxebooter/isomount 2>&1 | tee -a $OUTPUT
+
+    dialog --backtitle "done unmounting"\
+	 --title "UnMounting complete!" --clear\
+	 --msgbox "$(cat output.log)" 40 90
+}
+
 function viewlogs() {
 	dialog --textbox output.log 40 120
 }
 
+function setupNetworking() {
+
+	brctlinst=$(dpkg-query -W --showformat='${Status}\n' bridge-utils|grep "install ok installed")
+	if [ "" == "$brctlinst" ]; then
+		echo "No brctl. Installing"
+		baseTools
+	fi
+
+    dialog --title "What is the interface you want to use?"\
+     --backtitle "Interface" --clear\
+      --inputbox "Interface:" 8 60 2>$INPUT
+
+    response=$?
+
+	INTNAME=$(<$INPUT)
+
+    sudo ./pipework br0 $PXECID 192.168.242.1/24 2>&1 | tee -a $OUTPUT
+
+	#sudo brctl addif br0 $INTNAME 2>&1 | tee -a $OUTPUT
+
+	dialog --backtitle "done setting up networking"\
+	 --title "Network Setup Complete!" --clear\
+	 --msgbox "$(cat output.log)" 40 90
+}
+
+function runPXEbooter(){
+	dialog --backtitle "Starting PXE booter docker instance"\
+     --title "Booting PXE server......" --clear\
+      --infobox "Starting server, this could take a some time... :)" 40 90
+
+	sudo docker kill pxebooter 2>&1 | tee -a $OUTPUT
+	sudo docker rm pxebooter 2>&1 | tee -a $OUTPUT
+    PXECID=$(sudo docker run -d --name pxebooter --volume /tmp/data:/data -p 53:53 -p 26:26 --privileged pxeinstaller 2>&1 | tee -a $OUTPUT)
+
+    dialog --backtitle "PXE Booter Started: $PXECID"\
+	 --title "Please Run Setup Networking next!" --clear\
+	 --msgbox "$(docker logs pxebooter)" 40 90
+}
+
+function destroyPXEbooter(){
+
+	dialog --backtitle "Killing PXE booter docker instance"\
+     --title "Killing PXE server......" --clear\
+      --infobox "Killing server" 40 90
+
+	sudo docker kill pxebooter 2>&1 | tee -a $OUTPUT
+	sudo docker rm pxebooter 2>&1 | tee -a $OUTPUT
+
+    dialog --backtitle "PXE Booter Killed"\
+	 --title "Killed container" --clear\
+	 --msgbox "$(cat output.log)" 40 90
+
+}
 
 while true
 do
@@ -70,7 +136,11 @@ number keys 1-9 to choose an option.\n\n\
 Choose from the options below" 25 80 30 \
 downloadUbuntuServer "Run this first if you haven't downloaded the image" \
 MountLocalFiles "Mount Local Files" \
+unMountLocalFiles "UnMount Local Files" \
 createPXEbooter "Create PXE Booter" \
+runPXEbooter "Run PXE Booter" \
+setupNetworking "Setup Networking" \
+destroyPXEbooter "Destroy Instance" \
 ViewLog "View the installer log" \
 SaveLog "Save the installer log (exiting deletes logfile)" \
 Exit "Exit to the shell" 2>"${INPUT}"
@@ -80,7 +150,11 @@ menuitem=$(<"${INPUT}")
 case $menuitem in
 	downloadUbuntuServer) downloadUbuntuServer;;
 	MountLocalFiles) MountLocalFiles;;
+	unMountLocalFiles) unMountLocalFiles;;
 	createPXEbooter) createPXEbooter;;
+	runPXEbooter) runPXEbooter;;
+	setupNetworking) setupNetworking;;
+	destroyPXEbooter) destroyPXEbooter;;
 	ViewLog) viewlogs;;
 	SaveLog) cp $OUTPUT ./SavedLog.log;;
 	Exit) echo "Leaving the installer"; break;;
